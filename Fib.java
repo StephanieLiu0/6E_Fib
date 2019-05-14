54eb539ba0b56378d20d647598f2406fd39cc519
Stephanie Liu <sliu00@stuy.edu>
2019-05-13 22:30:59 -0400

    updated

--- Fib.java
+++ Fib.java
@@ -11,10 +11,16 @@ public class Fib {
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
+    public static long fib_recurrence( int n) {
+	if (n<=2)    // decision between base case and recursive case
+	    return 1;    // solution to base case
+	else if (n>2)    // solution to recursive case
+	    return fib_recurrence(n-1)+ fib_recurrence(n-2);
+	    // recursive abstraction + combining operator + recursive abstraction
         return -2; // invalid Fibonacci number
     }
      /* These are class / instance methods because...
+	method is not operating on a Fib object.
     */
 
 
@@ -22,18 +28,23 @@ public class Fib {
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_grade8( int n) {
+	int num, first, second;
+	for (num = 1, first = 1, second = 1;
+	     num < n;
+	     num++, second+=first, first=second) {
+	}
+	return second;
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be ... linear
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count... additions of first and second
        
-       Then cost of the the recurrence algorithm
-       grows ?? 
+       Then cost of the grade8 algorithm
+       grows linearly
        as the size of the problem increases,
-       because ??
+       because we add the previous two terms for each n value
      */
 
 
@@ -43,17 +54,20 @@ public class Fib {
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
-        return -2; // invalid Fibonacci number
+    public static double fib_Binet( int n) {
+	double phi = (1+Math.sqrt(5))/2;
+	double psi = (1-Math.sqrt(5))/2;
+
+	return ( (Math.pow(phi,n)) - (Math.pow(psi,n))) / Math.sqrt(5);
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be ... constant
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count... calculations
        
-       Then cost of the the recurrence algorithm
-       grows ?? 
+       Then cost of the Binet's formula algorithm
+       stays the same
        as the size of the problem increases,
-       because ??
+       because we plug in once and get an answer
      */
 }
